936b8445e5180f200f97361f9562c61b1f2054e3
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-10-20 11:33:34 -0500

    moved html elementsts from index.html to spa.shell.js

--- day3/js/spa.shell.js
+++ day3/js/spa.shell.js
@@ -1,7 +1,7 @@
 /*
  * Danilo Zekovic
  * spa.shell.js
- * Shell module for SPA
+ * Shell module for TipCalculator
 */
 
 /*jslint         browser : true, continue : true,
@@ -15,21 +15,71 @@
 spa.shell = (function () {
   //---------------- BEGIN MODULE SCOPE VARIABLES --------------
   var
-    /*configMap = {
+    configMap = {
       main_html : String()
-        + '<div class="spa-shell-head">'
-          + '<div class="spa-shell-head-logo"></div>'
-          + '<div class="spa-shell-head-acct"></div>'
-          + '<div class="spa-shell-head-search"></div>'
-        + '</div>'
-        + '<div class="spa-shell-main">'
-          + '<div class="spa-shell-main-nav"></div>'
-          + '<div class="spa-shell-main-content"></div>'
-        + '</div>'
-        + '<div class="spa-shell-foot"></div>'
-        + '<div class="spa-shell-chat"></div>'
-        + '<div class="spa-shell-modal"></div>'
-    },*/
+        
+        + '<div class="jumbotron">'
+          + '<h1>Tip Calculator</h1>'
+          + '<p>Calculate your tip and split it with your friends with just few clicks</p>' 
+        + '</div>' 
+        
+        + '<form class="form-horizontal" role="form">'
+          + '<!-- Input box for amount -->'
+          + '<div class="form-group">'
+            + '<label class="control-label col-sm-2" for="amount">Check amount:</label>'
+            + '<div class="col-sm-10">'
+              + '<input type="number" class="form-control" id="amount" min="0" placeholder="Amount in dollars">'
+            + '</div>'
+          + '</div>'
+          
+          + '<!-- Input box for number of people splittiong the check -->'
+          + '<div class="form-group">'
+            + '<label class="control-label col-sm-2" for="num-people">Number of people:</label>'
+            + '<div class="col-sm-10">'
+              + '<select class="form-control" id="num-people">'
+                + '<option>1</option>'
+                + '<option>2</option>'
+                + '<option>3</option>'
+                + '<option>4</option>'
+                + '<option>5</option>'
+                + '<option>6</option>'
+                + '<option>7</option>'
+                + '<option>8</option>'
+                + '<option>9</option>'
+                + '<option>10</option>'
+              + '</select>'
+            + '</div>'
+          + '</div>'
+          + '<div class="col-md-1">0%</div>'
+          + '<div class="col-md-10">'
+            + '<div id="slider"></div>'
+          + '</div>'
+          + '<div class="col-md-1">50% <p id="percent">20%</p></div>'
+          
+          + '<div class="form-group">'
+            + '<!-- Calculate button -->'
+            + '<div class="col-sm-4">'
+              + '<button type="button" class="btn btn-default pull-right">Calculate</button>'
+            + '</div>'
+          
+            + '<div class="col-sm-8">'
+              + '<table class="table table-hover">'
+                + '<tbody>'
+                  + '<tr>'
+                    + '<th>Total Tip:</th>'
+                    + '<td id="totalTip"> --tip amount-- </td>'
+                  + '</tr>'
+                  + '<tr>'
+                    + '<th>Per Person:</th>'
+                    + '<td id="perPerson"> --amount to pay per person-- </td>'
+                  + '</tr>'
+                + '</tbody>'
+              + '</table>'
+            + '</div>'
+          + '</div>'
+          
+        + '</form>'
+    },
     stateMap  = { $container : null },
     jqueryMap = {},
 
@@ -56,7 +106,7 @@ spa.shell = (function () {
   initModule = function ( $container ) {
     stateMap.$container = $container;
     console.log("It works");
-    //$container.html( configMap.main_html );
+    $container.html( configMap.main_html );
     setJqueryMap();
   };
   // End PUBLIC method /initModule/
